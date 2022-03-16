c553ca58f4e23c4f434200cac837958eba904544
obelixx1984 <karbowiakk@wp.pl>
2022-03-16 20:11:30 +0100

    Koniec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { StockInterface, StocksService } from './services/stocks.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  stocks: Array<StockInterface> = [];
-
-  constructor(private service: StocksService) {
-    this.service.load(['TWTR']).subscribe((stocks) => {
-      this.stocks = stocks;
-    });
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,22 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'
 import { StocksService } from './services/stocks.service';
 import { SummaryComponent } from './components/summary/summary.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SummaryComponent
+    SummaryComponent,
+    DashboardComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [StocksService],
   bootstrap: [AppComponent]
